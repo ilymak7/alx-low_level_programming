d4e8db5fe90ff90db655c2a25b2cb256bf8d70b3
ilymak7 <ilyass.makroz@gmail.com>
2023-03-17 15:31:33 -0700

    Task 2-3

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -11,12 +11,17 @@
 int main(void)
 {
 	char ch = 'a';
-
+	char CH = 'A';
 	while (ch <= 'z')
 	{
 		putchar(ch);
 		++ch;
 	}
+	while (CH <= 'Z')
+        {
+                putchar(CH);
+                ++CH;
+        }
 	putchar('\n');
 	return (0);
 }
