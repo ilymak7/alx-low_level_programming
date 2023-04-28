3c18d686d86b82cad44085705046438acc730ab1
ilymak7 <ilyass.makroz@gmail.com>
2023-04-27 22:08:34 -0700

    optional

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -1,9 +1,9 @@
 #ifndef LISTS_H
 #define LISTS_H
 
-#include <stdio.h> 
-#include <stdlib.h> 
-#include <string.h> 
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 /**
  * struct listint_s - singly linked list
@@ -34,5 +34,6 @@ listint_t *reverse_listint(listint_t **head);
 size_t print_listint_safe(const listint_t *head);
 listint_t *find_listint_loop(listint_t *head);
 int _putchar(char c);
+size_t free_listint_safe(listint_t **h);
 
 #endif
