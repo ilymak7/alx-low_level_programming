0db88037dd35c379f4843237839a3f6c59129c16
ilymak <makroz.ilyass@gmail.com>
2023-07-06 02:41:44 -0700

    Mandatory tasks

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,27 @@
+#include<main.h>
+
+/**
+ * binary_to_uint - converts a binary number to an unsigned int.
+ * @b:  pointing to a string of 0 and 1 chars.
+ * Return:  the converted number, or 0 if
+ * there is one or more chars in the string b that is not 0 or 1
+ * b is NULL
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int n = 0, p = 1;
+	int l;
+
+	if (b == '\0')
+		return (0);
+	for (l = 0; b[l];)
+		l++;
+	for (l -= 1; l >= 0; l--)
+	{
+		if (b[l] != '0' && b[l] != '1')
+			return (0);
+		n += (b[l] - '0') * p;
+		p *= 2;
+	}
+	return (n);
+}
---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,21 @@
+#include<main.h>
+
+/**
+ * flip_bits - a function that returns the number of bits you would
+ * need to flip to get from one number to another.
+ * @n: a number.
+ * @m: a number.
+ * Return: a number.
+ */
+
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	unsigned long int f = n ^ m, i = 0;
+
+	while (f > 0)
+	{
+		i += (f & 1);
+		f >>= 1;
+	}
+	return (i);
+}
